90740777ac9baa499f0d42070ee316bd4cf5b912
satishkumar <mandapallisatish64@gmail.com>
2019-10-11 16:17:50 -0500

    added code for quint.

--- test/app.test.js
+++ test/app.test.js
@@ -5,5 +5,5 @@ QUnit.test('TEST con', assert => {
   assert.equal(con(90, 3), 30, 'input2')
   assert.equal(con(100.25, 5), 20.05, 'input3')
   assert.equal(con(12,50), 0.24, 'input4')
-  assert.equal(con(20,4), 5, 'input5')
+  assert.equal(con(1,0), 5, 'input5')
 })
